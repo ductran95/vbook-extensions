48ba4344d7a913979a63401f3ea212204b97f3f2
Duc Tran <tv.duc95@gmail.com>
2021-05-10 14:43:08 +0700

    Update v4

--- manhwa18/src/detail.js
+++ manhwa18/src/detail.js
@@ -4,9 +4,10 @@ function execute(url) {
     return Response.success({
         name: doc.select("div.info-manga ul.manga-info > h1").text(),
         cover: doc.select("div.info-manga div.info-cover img.thumbnail").attr("src"),
-        author: doc.select("div.info-manga ul.manga-info > li:contains(Author) small").text(),
+        author: doc.select("div.info-manga ul.manga-info > li:contains(Author) small a").text(),
         description: doc.select("div.info-manga div.row:contains(Description) p").text(),
-        category: doc.select("div.info-manga ul.manga-info > li:contains(Genre) small").text(),
+        detail: doc.select("div.info-manga ul.manga-info").html(),
+        category: doc.select("div.info-manga ul.manga-info > li:contains(Genre) small a").text(),
         host: "https://manhwa18.com",
         nsfw: true
     });
