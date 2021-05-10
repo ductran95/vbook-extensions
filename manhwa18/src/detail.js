c54c4968a7a44a005895aefa9722bf55d008ab96
Duc Tran <tv.duc95@gmail.com>
2021-05-10 11:07:56 +0700

    Fix icon, detail

--- manhwa18/src/detail.js
+++ manhwa18/src/detail.js
@@ -4,9 +4,9 @@ function execute(url) {
     return Response.success({
         name: doc.select("div.info-manga ul.manga-info > h1").text(),
         cover: doc.select("div.info-manga div.info-cover img.thumbnail").attr("src"),
-        author: doc.select("div.info-manga ul.manga-info > li:nth-child(2) small").text(),
-        description: doc.select("div.info-manga div.row:nth-child(1) p").text(),
-        category: doc.select("div.info-manga ul.manga-info > li:nth-child(3) small").html(),
+        author: doc.select("div.info-manga ul.manga-info > li:nth-child(4) small").text(),
+        description: doc.select("div.info-manga div.row:nth-child(2) p").text(),
+        category: doc.select("div.info-manga ul.manga-info > li:nth-child(5) small").html(),
         host: "https://manhwa18.com",
         nsfw: true
     });
