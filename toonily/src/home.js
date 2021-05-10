04f30c1e6d952b9a0401f0ef19fbc299246e6964
purple_sunset <purple.sunshine.alone@gmail.com>
2021-05-10 16:54:30 +0700

    Update version

--- toonily/src/home.js
+++ toonily/src/home.js
@@ -1,7 +1,7 @@
 function execute() {
     return Response.success([
         {title: "New", input: "https://toonily.com/webtoons/?m_orderby=new-manga", script: "gen.js"},
-        {title: "Trnding", input: "https://toonily.com/webtoons/?m_orderby=trending", script: "gen.js"},
+        {title: "Trending", input: "https://toonily.com/webtoons/?m_orderby=trending", script: "gen.js"},
         {title: "Completed", input: "https://toonily.com/webtoon-tag/completed-webtoon/", script: "gen.js"}
     ]);
 }
