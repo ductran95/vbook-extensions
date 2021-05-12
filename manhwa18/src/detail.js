5a4d8fefc0b0eb7879d6f9b8e2d1f583eacb787a
purple_sunset <purple.sunshine.alone@gmail.com>
2021-05-12 17:38:07 +0700

    Update manhwa18

--- manhwa18/src/detail.js
+++ manhwa18/src/detail.js
@@ -1,13 +1,21 @@
 function execute(url) {
     const doc = Http.get(url).html()
 
+    var lineEls = doc.select("div.info-manga ul.manga-info li:gt(1)");
+    var lines = [];
+    for (var i = 0; i < lineEls.size(); i++) {
+        var lineEl = lineEls.get(i);
+        lines.push(lineEl.text());
+    }
+    var linesText = lines.join("\n");
+
     return Response.success({
         name: doc.select("div.info-manga ul.manga-info > h1").text(),
         cover: doc.select("div.info-manga div.info-cover img.thumbnail").attr("src"),
         author: doc.select("div.info-manga ul.manga-info > li:contains(Author) small a").text(),
         description: doc.select("div.info-manga div.row:contains(Description) p").text(),
         category: doc.select("div.info-manga ul.manga-info > li:contains(Genre) small a").text(),
-        detail: doc.select("div.info-manga ul.manga-info li:gt(1)").text(),
+        detail: linesText,
         host: "https://manhwa18.com",
         nsfw: true
     });
