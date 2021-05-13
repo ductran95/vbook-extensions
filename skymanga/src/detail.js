314f815cbc5afc5a4ba4a14f6f0829e1da4a825b
purple_sunset <purple.sunshine.alone@gmail.com>
2021-05-13 21:20:55 +0700

    Update skymanga

--- skymanga/src/detail.js
+++ skymanga/src/detail.js
@@ -2,12 +2,12 @@ function execute(url) {
     const doc = Http.get(url).html()
 
     return Response.success({
-        name: doc.select("div.profile-manga div.post-title > h1").text(),
-        cover: doc.select("div.profile-manga div.tab-summary div.summary_image a > img").attr("src").replace('\t','').replace('\n',''),
-        author: doc.select("div.profile-manga div.tab-summary div.summary_content div.post-content div.post-content_item:contains(Author) div.summary-content").text(),
-        description: doc.select("div.c-page-content div.description-summary").text(),
-        detail: doc.select("div.profile-manga div.tab-summary div.summary_content div.post-content div.post-content_item").html(),
-        category: doc.select("div.profile-manga div.tab-summary div.summary_content div.post-content div.post-content_item:contains(Genre) div.summary-content").text(),
+        name: doc.select(".post-title > h1").text(),
+        cover: doc.select(".summary_image a > img").attr("data-src").replace('\t','').replace('\n',''),
+        author: doc.select(".post-content_item:contains(Author) div.summary-content").text(),
+        description: doc.select(".summary__content.active").text(),
+        detail: doc.select(".post-content div.post-content_item").html(),
+        category: doc.select(".post-content_item:contains(Genre) div.summary-content").text(),
         host: "https://skymanga.co",
         nsfw: true
     });
