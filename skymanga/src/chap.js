07814b8ef513b8b7b78fcee5123c07c7d46cfbb3
purple_sunset <purple.sunshine.alone@gmail.com>
2021-05-13 22:36:16 +0700

    test

--- skymanga/src/chap.js
+++ skymanga/src/chap.js
@@ -1,5 +1,4 @@
 function execute(url) {
-    var resp = Http.get(url).string();
     var data = ["https://skymanga.co/wp-content/uploads/WP-manga/data/manga_5f47ed8a91b56/1c2d69c82e24514bd82915435257d072/1.jpg"];
 
     return Response.success(data);
