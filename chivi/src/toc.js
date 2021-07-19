772ede00212d1acb15538550155fa49c06baa99f
purple_sunset <purple.sunshine.alone@gmail.com>
2021-07-19 09:43:55 +0700

    test

--- chivi/src/toc.js
+++ chivi/src/toc.js
@@ -3,15 +3,41 @@ function execute(url) {
 
     const chapList = [];
 
+    chapList.push({
+        name: "url: " + url,
+        url: url,
+        host: "https://chivi.xyz"
+    });
+
     if (doc) {
-        var el = doc.select(".chlist").last().select(".list > li > a")
+        chapList.push({
+            name: "doc ok",
+            url: url,
+            host: "https://chivi.xyz"
+        });
+
+        var chlist = doc.select(".chlist")
+
+        chapList.push({
+            name: "chlist: " + chlist.size(),
+            url: url,
+            host: "https://chivi.xyz"
+        });
+
+        var el = chlist.last().select(".list > li > a")
+
+        chapList.push({
+            name: "el: " + el.size(),
+            url: url,
+            host: "https://chivi.xyz"
+        });
 
         for (var i = 0; i < el.size(); i++) {
             var e = el.get(i);
             chapList.push({
-                "name": e.select(".title").text(),
-                "url": e.attr("href"),
-                "host": "https://chivi.xyz"
+                name: e.select(".title").text(),
+                url: e.attr("href"),
+                host: "https://chivi.xyz"
             });
         }
     }
