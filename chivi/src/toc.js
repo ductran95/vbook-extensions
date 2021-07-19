9d531ae6cb8e0848e0c53603d590e4e8bf5f3dcc
purple_sunset <purple.sunshine.alone@gmail.com>
2021-07-19 09:35:30 +0700

    test

--- chivi/src/toc.js
+++ chivi/src/toc.js
@@ -1,27 +1,20 @@
 function execute(url) {
-    // var doc = Http.get(url).html();
+    var doc = Http.get(url).html();
 
     const chapList = [];
 
-    chapList.push({
-        name: "url: " + url,
-        url: url,
-        host: "https://chivi.xyz"
-    });
+    if (doc) {
+        var el = doc.select(".chlist").last().select(".list > li > a")
 
-    // if (doc) {
-    //     const chapList = [];
-    //     var el = doc.select(".chlist").last().select(".list > li > a")
-
-    //     for (var i = 0; i < el.size(); i++) {
-    //         var e = el.get(i);
-    //         chapList.push({
-    //             "name": e.select(".title").text(),
-    //             "url": e.attr("href"),
-    //             "host": "https://chivi.xyz"
-    //         });
-    //     }
-    // }
+        for (var i = 0; i < el.size(); i++) {
+            var e = el.get(i);
+            chapList.push({
+                "name": e.select(".title").text(),
+                "url": e.attr("href"),
+                "host": "https://chivi.xyz"
+            });
+        }
+    }
 
     return Response.success(chapList);
 }
