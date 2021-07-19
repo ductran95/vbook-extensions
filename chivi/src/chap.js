128116e8714bfe3573dadad12db439f57261c67a
purple_sunset <purple.sunshine.alone@gmail.com>
2021-07-19 11:11:15 +0700

    Update chivi

--- chivi/src/chap.js
+++ chivi/src/chap.js
@@ -1,5 +1,6 @@
 function execute(url) {
     var doc = Http.get(url).html();
     doc.select("h1").remove();
+    doc.select(".adsbygoogle").remove();
     return Response.success(doc.select("article").html());
 }
