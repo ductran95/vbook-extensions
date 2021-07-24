f816b5aef6d6d21fd5ce3617da844acfa8fa1020
purple_sunset <purple.sunshine.alone@gmail.com>
2021-07-24 23:24:27 +0700

    Update

--- chivi/src/toc.js
+++ chivi/src/toc.js
@@ -1,26 +1,48 @@
 function execute(url) {
-    var doc = Http.get(url).html();
+    var chapUrl = url + "/chaps";
+    var doc = Http.get(chapUrl).html();
 
     const chapList = [];
 
-    var source = "";
-
-    chapList.push({
-        name: url,
-        url: url,
-        host: "https://chivi.xyz"
-    });
-
     if (doc) {
-        var el = doc.select(".chlist").select(".list").last().select("li > a")
+        var source = doc.select(".source ._active").text().toLowerCase()
 
-        for (var i = 0; i < el.size(); i++) {
-            var e = el.get(i);
-            chapList.push({
-                name: e.select(".title").text(),
-                url: e.attr("href"),
-                host: "https://chivi.xyz"
-            });
+        var pages = doc.select(".pagi a._line");
+        var lastPage = pages.last().attr("href");
+
+        const pageRegex = /.*page=(\d+)/g;
+        const result = pageRegex.exec(lastPage);
+        if (result) {
+            var lastPageNo = parseInt(result[1]);
+            for (var i = 1; i <= lastPageNo; i++) {
+
+                var chapDoc = Http.get(chapUrl + "/" + source + "?page=" + i).html();
+                
+                if (chapDoc) {
+                    var el = chapDoc.select(".chlist").select(".list").last().select("li > a")
+            
+                    for (var i = 0; i < el.size(); i++) {
+                        var e = el.get(i);
+                        chapList.push({
+                            name: e.select(".title").text(),
+                            url: e.attr("href"),
+                            host: "https://chivi.xyz"
+                        });
+                    }
+                }
+            }
+        }
+        else {
+            var el = doc.select(".chlist").select(".list").last().select("li > a")
+
+            for (var i = 0; i < el.size(); i++) {
+                var e = el.get(i);
+                chapList.push({
+                    name: e.select(".title").text(),
+                    url: e.attr("href"),
+                    host: "https://chivi.xyz"
+                });
+            }
         }
     }
 
