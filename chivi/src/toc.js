1efcf71708138b4dec1a0d28ebb59f927f08f15a
Duc Tran <tv.duc95@gmail.com>
2023-05-08 21:05:27 +0700

    Update chivi

--- chivi/src/toc.js
+++ chivi/src/toc.js
@@ -4,13 +4,25 @@ function execute(url) {
         let doc = response.json();
         
         let pageNo = 1;
-        let tocUrl = "/_wn/chaps/" + doc.id 
+        let tocUrl = "https://chivi.app/_wn/chaps/" + doc.id;
+
+        chapList.push({
+            "name": tocUrl,
+            "url": tocUrl,
+            "host": "https://chivi.app"
+        });
         
-        let chapList = []
+        let chapList = [];
         let hasChap = true;
         while(hasChap) {
             let tocResponse = fetch(tocUrl + "/_?pg=" + pageNo);
             if (tocResponse.ok) {
+                chapList.push({
+                    "name": tocResponse,
+                    "url": tocUrl,
+                    "host": "https://chivi.app"
+                });
+
                 let data = tocResponse.json();
                 hasChap = data.length > 0;
 
@@ -32,5 +44,5 @@ function execute(url) {
         return Response.success(chapList);
     }
 
-    return null
+    return null;
 }
