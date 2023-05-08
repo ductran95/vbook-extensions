ea0c088c28e71ead28a040ea7f2a2eccc8ac61ad
Duc Tran <tv.duc95@gmail.com>
2023-05-08 21:48:11 +0700

    Update chivi

--- chivi/src/chap.js
+++ chivi/src/chap.js
@@ -3,15 +3,8 @@ function execute(url) {
 
     if (response.ok) {
         let doc = response.html();
-        let article = doc.select("article.article").first();
-        let section = article.select("section").first();
-
-        if(section != null) {
-            return Response.success(section.html());
-        }
-        else {
-            return Response.success(article.html());
-        }
+        doc.select("article.article section h1#L0").empty();
+        return Response.success(doc.select("article.article section").html());
     }
 
     return null;
