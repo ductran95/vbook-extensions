e45ff118cc648e41d7cc10ae1d9ccae74d2e8172
purple_sunset <purple.sunshine.alone@gmail.com>
2023-05-09 16:54:58 +0700

    Update

--- chivi/src/chap.js
+++ chivi/src/chap.js
@@ -2,17 +2,26 @@ function execute(url) {
   let response = fetch(url);
 
   if (response.ok) {
-      let text = response.text();
-      text = text
-      .replaceAll(/<v-n[a-zA-Z0-9\s\-\=]*>/ig, "")
-      .replaceAll('</v-n>', "")
-      .replaceAll(/<v-g[a-zA-Z0-9\s\-\=]*>/ig, "")
-      .replaceAll('</v-g>', "");
+    let text = response.text().toString();
+    text = text
+    .replaceAll(/<v-n[a-zA-Z0-9\s\-\=]*>/ig, "")
+    .replaceAll('</v-n>', "")
+    .replaceAll(/<v-g[a-zA-Z0-9\s\-\=]*>/ig, "")
+    .replaceAll('</v-g>', "");
 
-      let doc = Html.parse(text);
-      doc.select("article.article section h1#L0").remove();
-      return Response.success(doc.select("article.article section").html());
+    let doc = Html.parse(text);
+    doc.select("article.article section h1#L0").remove();
+    return Response.success(doc.select("article.article section").html());
   }
+
+  // if (response.ok) {
+  //   let doc = response.html();
+    
+  //   doc.select("article.article section h1#L0").remove();
+  //   doc.select("article.article section v-n").unwrap();
+  //   doc.select("article.article section v-g").unwrap();
+  //   return Response.success(doc.select("article.article section").html());
+  // }
 
   return null;
 }
