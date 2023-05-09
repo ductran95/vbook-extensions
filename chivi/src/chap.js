57bae91f2135557ffaa5a739c28b71faaee2ec80
purple_sunset <purple.sunshine.alone@gmail.com>
2023-05-09 16:47:04 +0700

    Update chivi

--- chivi/src/chap.js
+++ chivi/src/chap.js
@@ -4,14 +4,9 @@ function execute(url) {
   if (response.ok) {
       let doc = response.html();
       doc.select("article.article section h1#L0").remove();
-      let chapData = doc.select("article.article section").html();
-      chapData = chapData
-      .replaceAll(/<v-n[a-zA-Z0-9\s\-\=]*>/ig, "")
-      .replaceAll('</v-n>', "")
-      .replaceAll(/<v-g[a-zA-Z0-9\s\-\=]*>/ig, "")
-      .replaceAll('</v-g>', "")
-      ;
-      return Response.success(chapData);
+      doc.select("article.article section v-n").unwrap();
+      doc.select("article.article section v-g").unwrap();
+      return Response.success(doc.select("article.article section").html());
   }
 
   return null;
